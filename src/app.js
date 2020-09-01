c2de916b1d370d5b9f18759df5eea9099bb8f819
Fiyinfoluwa Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-09-01 11:10:30 +0100

    #174566170 Set up the development environment (#6)
    chore(env): setup dev environment
    
    - install express and dotenv
    
    [Delivers #174566170]

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/',(req, res) => {
+    res.send('Welcome to Know Africa');
+});
+
+app.listen(port, () => {
+    console.log(`Server Running on: ${port}`);
+});
