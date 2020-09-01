2a78bef51ff5340c6f061428b3c2380fcdfa7a78
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-01 12:36:19 +0200

    chore(babel): configure babel to transpile code
    - install @babe/node @babel/core @babel/preset-env @babel/cli
    - create .babelrc file and add configurations
    - add 3000 as default port for express server
    
    [Delivers #174566480]

--- src/app.js
+++ src/app.js
@@ -1,12 +1,12 @@
-import express from 'express';
+import express from "express";
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
-app.get('/',(req, res) => {
-    res.send('Welcome to Know Africa');
+app.get("/", (req, res) => {
+  res.send("Welcome to Know Africa");
 });
 
 app.listen(port, () => {
-    console.log(`Server Running on: ${port}`);
+  console.log(`Server Running on: ${port}`);
 });
