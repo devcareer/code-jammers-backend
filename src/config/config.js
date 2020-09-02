0061411ede133e58bd331c6b6c035411561a29ff
donaldcrane <donaldagbakoba@gmail.com>
2020-09-02 17:10:06 +0100

    chore(env): setup development enviroment
    - install express and dotenv
    
    [Delivers #174566483]

---
+++ src/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DB,
+    host: process.env.HOST,
+    dialect: 'postgres'
+  }
+};
