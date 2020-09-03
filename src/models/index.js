ef25d771a5f7998587458ef72eee073d4e005ea4
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-03 14:38:46 +0200

    feature(tourist-city-models) - create models
    - create city model
    - create tourist center model
    - create city migrations
    - create tourist centeer migrations
    - create city seeders
    - create tourist center seeders
    - install sequelize
    - install sequelize-cli
    
    [finishes #174626782]

--- src/models/index.js
+++ src/models/index.js
@@ -1,13 +1,8 @@
-import fs from "fs";
-
-import path from "path";
-
-import Sequelize from "sequelize";
-
-import configurations from "../database/config";
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
 
 const basename = path.basename(__filename);
-
 const env = process.env.NODE_ENV || "development";
 const config = require(`${__dirname}/../database/config/config.js`)[env];
 
@@ -18,27 +13,25 @@ if (config.url) {
 } else {
   sequelize = new Sequelize(
     config.database,
-
     config.username,
-
     config.password,
-    config,
+    config
   );
 }
 fs.readdirSync(__dirname)
   .filter(
-    file => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js",
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
-  .forEach(file => {
+  .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
 });
 db.sequelize = sequelize;
-
 db.Sequelize = Sequelize;
 module.exports = db;
