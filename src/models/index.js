fb011fc419c852e4d4bd0e547f305ff9bf842081
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-03 14:38:46 +0200

    feature(tourist-city-models) - create models
    - create city model
    - create tourist center model
    - create city migrations
    - create tourist centeer migrations
    - create city seeders
    - create tourist center seeders
    - install sequelize
    - install sequelize-cli
    
    [finishes #174626782]

--- src/models/index.js
+++ src/models/index.js
@@ -38,9 +38,11 @@ if (config.url) {
   );
 }
 fs.readdirSync(__dirname)
+
   .filter(
     (file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js",
   )
+
   .forEach((file) => {
 <<<<<<< HEAD
     const model = sequelize.import(path.join(__dirname, file));
