b6b3bdfc4e740a170bcca5ce7b471d38e8f9cf6a
donaldcrane <donaldagbakoba@gmail.com>
2020-09-03 16:57:20 +0100

    resolved conflict

--- src/database/index.js
+++ src/database/index.js
@@ -1,25 +1,27 @@
-require('dotenv').config();
+import dotenv from "dotenv";
+
+dotenv.config();
 
 module.exports = {
-  development: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB,
-    host: process.env.HOST,
-    dialect: 'postgres'
-  },
-  test: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB,
-    host: process.env.HOST,
-    dialect: 'postgres'
-  },
-  production: {
-    username: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DB,
-    host: process.env.HOST,
-    dialect: 'postgres'
-  }
+	development: {
+		username: process.env.USER,
+		password: process.env.PASSWORD,
+		database: process.env.DB,
+		host: process.env.HOST,
+		dialect: "postgres",
+	},
+	test: {
+		username: process.env.USER,
+		password: process.env.PASSWORD,
+		database: process.env.DB,
+		host: process.env.HOST,
+		dialect: "postgres",
+	},
+	production: {
+		username: process.env.USER,
+		password: process.env.PASSWORD,
+		database: process.env.DB,
+		host: process.env.HOST,
+		dialect: "postgres",
+	},
 };
