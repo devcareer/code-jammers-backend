f601981c7896c36691938e0e6f6240d394ae16be
Fiyinfoluwa Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-09-04 16:43:10 +0100

    #174566485 Set up CI and CD and add badges to readMe (#9)
    chore(ci/cd): setup CI/CD and add badges
    
    - add repo to codeclimate
    - add github actions
    - add badges
    
    [Delivers #174566485]

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -1,8 +1,6 @@
-
 import chai from "chai";
 import chaiHttp from "chai-http";
 import server from "../app";
-
 
 // assertion style
 
@@ -11,18 +9,15 @@ chai.should();
 chai.use(chaiHttp);
 
 describe("app.js should return all endpoints", async () => {
-	describe("/ should display Welcome to Know Africa", () => {
-		it("it should get the welcome page", (done) => {
-			chai
-				.request(server)
-				.get("/")
-				.end((err, res) => {
-					res.should.have.status(200);
-					done();
-				});
-		});
-	});
-    
+  describe("/ should display Welcome to Know Africa", () => {
+    it("it should get the welcome page", (done) => {
+      chai
+        .request(server)
+        .get("/")
+        .end((err, res) => {
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
 });
-
-export default describe;
