b3dfde5cf7aae8e6dc072dc8c7a15ea2e4311df7
AbonyiXavier <francisabonyi@gmail.com>
2020-09-04 18:45:08 +0100

    feat(countries-culture-model) - create models
    - Done creating country and culture migrations, models and seeders
    
    [Delivers #174626687]

---
+++ src/seeders/20200904135720-countries.js
@@ -0,0 +1,39 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     */
+
+    await queryInterface.bulkInsert(
+      "Countries",
+      [
+        {
+          namesOfCountries: "Nigeria",
+          gallery:
+            "https://img.freepik.com/free-vector/nigeria-map-flag-national-emblem_2239-230.jpg?size=338&ext=jpg",
+          capital: "FCT Abuja",
+          population: 205,
+          officialLanguage: "English",
+          region: "West Africa",
+          currency: "Naira",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     */
+    await queryInterface.bulkDelete("Countries", null, {});
+  },
+};
