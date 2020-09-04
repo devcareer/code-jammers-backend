79ebfa65752ddd86cb6166f982385cd537adc85d
donaldcrane <donaldagbakoba@gmail.com>
2020-09-04 12:05:47 +0100

    modified eslint

--- src/database/config.js
+++ src/database/config.js
@@ -3,17 +3,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 module.exports = {
-	development: {
-		url: process.env.DATABASE_URL,
-		dialect: "postgres",
-	},
-	test: {
-		url: process.env.DATABASE_URL,
-		dialect: "postgres",
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+  },
+  test: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
 
-	},
-	production: {
-		url: process.env.DATABASE_URL,
-		dialect: "postgres",
-	},
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+  },
 };
