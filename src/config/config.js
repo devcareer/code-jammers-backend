1d076367683ccf185c802e2823db9540ff0a74a3
Augusta Ehihebolo <ehiheboloaugustar@gmail.com>
2020-09-07 17:12:33 +0100

    feat(models): create music and food models
    -create sequelize models with migrations and seeders for music and food
    
    [Delivers #174626768]

--- src/config/config.js
+++ src/config/config.js
@@ -1,24 +1,25 @@
-require(“dotenv”).config();
+require("dotenv").config();
+
 module.exports = {
   development: {
-    username": "postgres",
-      "password": 1234,
-      "database": "knowafrica",
-      "host": "127.0.0.1",
-    dialect: “postgres”,
+    username: "postgres",
+    password: 1234,
+    database: "knowafrica",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
   test: {
-    username": "postgres",
-      "password": 1234,
-      "database": "knowafrica",
-      "host": "127.0.0.1",
-    dialect: “postgres”,
+    username: "postgres",
+    password: 1234,
+    database: "knowafrica",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
   production: {
-    "username": "postgres",
-    "password": 1234,
-    "database": "knowafrica",
-    "host": "127.0.0.1",
-    "dialect": "postgres",
+    username: "postgres",
+    password: 1234,
+    database: "knowafrica",
+    host: "127.0.0.1",
+    dialect: "postgres",
   },
 };
--- src/models/index.js
+++ src/models/index.js
@@ -1,4 +1,3 @@
-
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 const fs = require("fs");
@@ -8,8 +7,6 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(`${__dirname}/../config/config.js`)[env];
-
-
 const db = {};
 
 let sequelize;
@@ -23,13 +20,11 @@ fs
   .readdirSync(__dirname)
   .filter((file) => (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js"))
   .forEach((file) => {
-
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
 Object.keys(db).forEach((modelName) => {
-
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
