28f6d27ffc52598ee3cca181362553706ab4f32e
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-07 16:32:04 +0200

    feature(tourist-city-models) - create models
    - install sinon
    - install sinon-chai
    - install sequelize-test-helpers
    - change test script to run all tests in tests folder
    - write tests for city model
    - write tests for tourist center model
    
    [finishes #174626782]

---
+++ src/tests/unit/models/City.spec.js
@@ -0,0 +1,37 @@
+import chai from "chai";
+import sinonChai from "sinon-chai";
+
+chai.use(sinonChai);
+
+const { expect } = chai;
+
+const {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkPropertyExists,
+} = require("sequelize-test-helpers");
+
+const cityModel = require("../../../models/city");
+
+describe("src/models/City", () => {
+  const City = cityModel(sequelize, dataTypes);
+  const city = new City();
+  checkModelName(City)("City");
+
+  context("properties", () => {
+    ["id", "location", "gallary", "name"].forEach(checkPropertyExists(city));
+  });
+
+  context("associations", () => {
+    const Country = "Zambia";
+
+    before(() => {
+      City.associate({ Country });
+    });
+
+    it("defined a belongsTo association with Country", () => {
+      expect(City.belongsTo).to.have.been.calledWith(Country);
+    });
+  });
+});
