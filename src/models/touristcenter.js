948b53bbbccc253868619eceda1a886360f74024
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-07 16:32:04 +0200

    feature(tourist-city-models) - create models
    - install sinon
    - install sinon-chai
    - install sequelize-test-helpers
    - change test script to run all tests in tests folder
    - write tests for city model
    - write tests for tourist center model
    
    [finishes #174626782]

--- src/models/touristcenter.js
+++ src/models/touristcenter.js
@@ -1,5 +1,15 @@
 module.exports = (sequelize, DataTypes) => {
+<<<<<<< HEAD
   const TouristCenter = sequelize.define("TouristCenters", {
+=======
+  const TouristCenter = sequelize.define("TouristCenter", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+>>>>>>> 29193c4... feature(tourist-city-models) - create models
     name: {
       type: DataTypes.STRING,
       allowNull: false,
