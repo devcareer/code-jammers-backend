ac16d9dd50caef687a7c09d0c6ad3e1662d88491
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-07 16:32:04 +0200

    feature(tourist-city-models) - create models
    - install sinon
    - install sinon-chai
    - install sequelize-test-helpers
    - change test script to run all tests in tests folder
    - write tests for city model
    - write tests for tourist center model
    
    [finishes #174626782]

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -10,7 +10,6 @@ chai.should();
 chai.use(chaiHttp);
 
 describe("app.js should return all endpoints", async () => {
-<<<<<<< HEAD
   describe("/ should display Welcome to Know Africa", () => {
     it("it should get the welcome page", (done) => {
       chai
@@ -23,19 +22,3 @@ describe("app.js should return all endpoints", async () => {
     });
   });
 });
-=======
-	describe("/ should display Welcome to Know Africa", () => {
-		it("it should get the welcome page", (done) => {
-			chai
-				.request(server)
-				.get("/")
-				.end((err, res) => {
-					res.should.have.status(200);
-					done();
-				});
-		});
-	});
-});
-
-export default describe;
->>>>>>> feature(tourist-city-models) - create models
