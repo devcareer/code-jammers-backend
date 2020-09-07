65431e97991ee01e384d771c36ad2fbdca09e2e8
AbonyiXavier <francisabonyi@gmail.com>
2020-09-07 12:47:04 -0700

    fix(Country-culture-models) - create models
    - fixed migrations, models and seeders
    
    [Deliver #174626687]

--- src/migrations/20200903144113-create-countries.js
+++ src/migrations/20200903144113-create-countries.js
@@ -1,7 +1,7 @@
 "use strict";
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Country", {
+    await queryInterface.createTable("Countries", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -40,6 +40,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Country");
+    await queryInterface.dropTable("Countries");
   },
 };
--- src/seeders/20200904135720-countries.js
+++ src/seeders/20200904135720-countries.js
@@ -9,7 +9,7 @@ module.exports = {
      */
 
     await queryInterface.bulkInsert(
-      "Country",
+      "Countries",
       [
         {
           nameOfCountry: "Nigeria",
@@ -34,6 +34,6 @@ module.exports = {
      *
      * Example:
      */
-    await queryInterface.bulkDelete("Country", null, {});
+    await queryInterface.bulkDelete("Countries", null, {});
   },
 };
