0f089a2811c82bcaf8699f97e2cd24b733b4d148
unknown <ufuomaogodo@gmail.com>
2020-09-07 14:18:20 +0100

    lint code

--- src/models/index.js
+++ src/models/index.js
@@ -8,9 +8,8 @@ const env = process.env.NODE_ENV || "development";
 const config = require(`${__dirname}/../database/config/config.js`)[env];
 const db = {};
 let sequelize;
-if (config.url) {
-  sequelize = new Sequelize(config.url, config);
-  sequelize = new Sequelize(process.env[config.url], config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database,
@@ -31,7 +30,6 @@ Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
-
 });
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
