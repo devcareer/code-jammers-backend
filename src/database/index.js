e4da5520a9f4f8ed3a2461df34e1a1c8e3ccfcef
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-08 12:12:15 +0200

    feature(tourist-city-models) - create city and tourist center models
    - move configuration for db from config to datbase foler
    
    [finishes #174626782]

---
+++ src/database/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+  },
+  test: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: "postgres",
+  },
+};
