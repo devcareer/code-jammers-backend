33a8a77d59a70cc6c94ad5e7c47a5a976450260a
Ufuoma Ogodo <ufuomaogodo@gmail.com>
2020-09-08 08:39:55 +0100

    add seq-test script test migrations with sequelize test helpers

---
+++ src/tests/securityinformation.test.js
@@ -0,0 +1,35 @@
+import {
+  sequelize,
+  dataTypes,
+  checkModelName,
+  checkUniqueIndex,
+  checkPropertyExists,
+  makeMockModels,
+} from "sequelize-test-helpers";
+
+import SecurityinformationModel from "../models/securityinformation";
+
+describe("src/models/securityinformation", () => {
+  const Securityinformation = SecurityinformationModel(sequelize, dataTypes);
+  const securityinformation = new Securityinformation();
+
+  checkModelName(Securityinformation)("Securityinformation");
+
+  context("properties", () => {
+    ["countryId", "location"].forEach(
+      checkPropertyExists(securityinformation),
+    );
+  });
+
+  //   context("associations", () => {
+  //     const Country = "Nigeria";
+
+  //     before(() => {
+  //       Securityinformation.associate({ Country });
+  //     });
+
+  //     it("defined a belongsTo association with Country", () => {
+  //       expect(Securityinformation.belongsTo).to.have.been.calledWith(Country);
+  //     });
+  //   });
+});
