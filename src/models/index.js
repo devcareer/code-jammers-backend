725320eee68c6a8c3858cdff8676bd50d75b6602
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-08 12:12:15 +0200

    feature(tourist-city-models) - create city and tourist center models
    - move configuration for db from config to datbase foler
    
    [finishes #174626782]

--- src/models/index.js
+++ src/models/index.js
@@ -21,11 +21,10 @@ if (config.url) {
 }
 
 fs.readdirSync(__dirname)
-
   .filter(
-    (file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js",
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
   )
-
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
