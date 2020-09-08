96e99d2e72ac96e148aee22efcdf0363b56c6217
Bislon Zulu <bislonzulu1@outlook.com>
2020-09-08 16:46:55 +0200

    #174566179 Prepare app for heroku deployment - [staging] (#20)
    chore(procfile) -configured procfile
    
    - Added build script to build the app locally before deploying
    - Created a Heroku app
    - Configured Heroku app to work with nodejs
    
    [Delivers #174566179]

--- src/app.js
+++ src/app.js
@@ -4,11 +4,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-	res.send("Welcome to Know Africa");
+  res.send("Welcome to Know Africa");
 });
 
 app.listen(port, () => {
-	console.log(`Server Running on: ${port}`);
+  console.log(`Server Running on: ${port}`);
 });
 
 export default app;
