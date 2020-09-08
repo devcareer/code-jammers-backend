4cc11dc731d8c15074dc36a2337f7b9bd474dfa5
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-08 12:12:15 +0200

    feature(tourist-city-models) - create city and tourist center models
    - move configuration for db from config to datbase folder
    
    [finishes #174626782]

--- src/models/index.js
+++ src/models/index.js
@@ -26,20 +26,19 @@ if (config.use_env_variable) {
 
     config.password,
 
-    config
+    config,
   );
 }
 
 fs.readdirSync(__dirname)
   .filter(
-    (file) =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    (file) => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js",
   )
   .forEach((file) => {
     const model = require(path.join(__dirname, file))(
       sequelize,
 
-      Sequelize.DataTypes
+      Sequelize.DataTypes,
     );
     db[model.name] = model;
   });
