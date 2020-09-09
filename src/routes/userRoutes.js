caaa7b15211e21d6554e79b52bf0038ca8fa42bb
Uche <godspowercuche56@gmail.com>
2020-09-09 16:22:17 +0100

    feature(setup):crea user account

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,7 +2,6 @@ import { Router } from "express";
 import userController from "../controllers/user";
 
 const router = Router();
-console.log(userController.createUser);
 router.post("/users/signup", userController.createUser);
 
 export default router;
