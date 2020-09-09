c35d8aa5b8d15cd4d6d8655e93a0759939326442
mdcodes <mdcodes2308@gmail.com>
2020-09-09 13:44:45 +0100

    testing

--- src/tests/models/profile.js
+++ src/tests/models/profile.js
@@ -1,30 +1,37 @@
-import { expect } from 'chai'
+import chai from "chai";
+import { expect } from "chai";
+import sinonChai from "sinon-chai";
+chai.use(sinonChai);
+
 import {
   sequelize,
   dataTypes,
   checkModelName,
   checkUniqueIndex,
-  checkPropertyExists 
-} from 'sequelize-test-helpers'
-import ProfileModel from '../../models/profile'
-describe('src/models/Profile', () => {
+  checkPropertyExists
+} from "sequelize-test-helpers";
+
+import ProfileModel from "../../models/profile"
+describe("src/models/Profile", () => {
   const Profile = ProfileModel(sequelize, dataTypes)
   const profile = new Profile()
-  checkModelName(Profile)('Profile')
-  context('properties', () => {
-    ;['firstName', 'lastName', 'profilePicture'].forEach(
+  checkModelName(Profile)(Profile)
+  context("properties", () => {
+    ;["firstName", "lastName", "profilePicture", "userId"].forEach(
       checkPropertyExists(profile)
     )
   })
-  context('associations', () => {
-    const User = 'some dummy User'
+  context("associations", () => {
+    const User = "some dummy profile"
     before(() => {
       Profile.associate({ User })
     })
-    it('defined a belongsTo association with Company', () => {
-      expect(Profile.belongsTo).to.have.been.calledWith(User, {
-          foreignKey: 'userId',
-      })
-    })
+    it("defined a belongsTo association with User", () => {
+      expect(Profile.belongsTo).to.have.been.calledWith(User);
+    });
   })
 })
+
+
+
+
--- src/utilities/saveme.js
+++
@@ -1,9 +0,0 @@
-const { User } = require('../models')
- 
-const saveMe = async ({ id, ...data }) => {
-  const user = await User.findOne({ where: { id } })
-  if (user) return await user.update(data)
-  return null
-}
- 
-module.exports = saveMe
