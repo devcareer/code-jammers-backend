73726ff02643ff64c5ee4e414dcb8c5691f40466
unknown <ufuomaogodo@gmail.com>
2020-09-09 16:51:47 +0100

    rebase develop

---
+++ src/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: "postgres",
+    password: 1234,
+    database: "knowafrica",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+  },
+};
