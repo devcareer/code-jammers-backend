c5c8a699a3363d8f298e8c851510d6f1f5fd2a1c
mdcodes <mdcodes2308@gmail.com>
2020-09-11 16:19:23 +0100

    fix conflict

--- src/app.js
+++ src/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import express from "express";
 
 const app = express();
@@ -13,19 +12,3 @@ app.listen(port, () => {
 });
 
 export default app;
-=======
-import express from "express";
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Welcome to Know Africa");
-});
-
-app.listen(port, () => {
-  console.log(`Server Running on: ${port}`);
-});
-
-export default app;
->>>>>>> reviews
