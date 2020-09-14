b540998e42f286433a5ab4fedd49a7d28ec10710
netae <ehiheboloaugustar@gmail.com>
2020-09-14 04:32:33 +0100

    resolve conflict

--- src/models/food.js
+++ src/models/food.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = (sequelize, DataTypes) => {
   const Culture = sequelize.define("Food", {
     id: {
@@ -33,3 +34,28 @@ module.exports = (sequelize, DataTypes) => {
   };
   return Food;
 };
+=======
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Food extends Model {
+   
+    static associate(models) {
+    
+    }
+  };
+  Food.init({
+    id: DataTypes.INTEGER,
+    countryId: DataTypes.INTEGER,
+    type: DataTypes.STRING,
+    gallery: DataTypes.STRING,
+    event: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Food',
+  });
+  return Food;
+};
+>>>>>>> resolve conflict
--- src/models/index.js
+++ src/models/index.js
@@ -24,6 +24,7 @@ fs
   .filter(file => (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js"))
   .forEach(file => {
 
+
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
