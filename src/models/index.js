d7865c9e46c45574cbac8c26fcff84f34e8a1103
AbonyiXavier <francisabonyi@gmail.com>
2020-09-14 15:32:15 +0100

    error fixed

--- src/models/index.js
+++ src/models/index.js
@@ -1,48 +1,36 @@
-"use strict";
-
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(__dirname + "/../config/config.js")[env];
 
+const config = require(`${__dirname}/../database/config.js`)[env];
 const db = {};
 let sequelize;
 if (config.url) {
   sequelize = new Sequelize(config.url, config);
   sequelize = new Sequelize(process.env[config.url], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
+
 fs.readdirSync(__dirname)
-<<<<<<< HEAD
-  .filter((file) => {
-    return (
+  .filter(
+    (file) =>
       file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-<<<<<<< HEAD
-    );
-  })
+  )
   .forEach((file) => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
-Object.keys(db).forEach((modelName) => {
-=======
-=======
-  .filter(
-    file =>
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js",
->>>>>>> 1a6a907... error fixed
-  )
-  .forEach(file => {
-    const model = sequelize.import(path.join(__dirname, file));
-    db[model.name] = model;
-  });
 
-Object.keys(db).forEach(modelName => {
->>>>>>> 0646013... error fixed
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
