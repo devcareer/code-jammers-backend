00d7ec0a717a3f6038c1d56f47ed82128826b7da
AbonyiXavier <francisabonyi@gmail.com>
2020-09-14 14:59:05 +0100

    error fixed

--- src/models/index.js
+++ src/models/index.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/../config/config.js")[env];
