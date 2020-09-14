03aab5db96b913da7ee308518ea072acb8bde43c
netae <ehiheboloaugustar@gmail.com>
2020-09-14 04:25:02 +0100

    resolve conflict

--- src/models/index.js
+++ src/models/index.js
@@ -7,6 +7,8 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(`${__dirname}/../config/config.js`)[env];
+
+
 const db = {};
 
 let sequelize;
@@ -18,13 +20,16 @@ if (config.use_env_variable) {
 
 fs
   .readdirSync(__dirname)
+
   .filter(file => (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js"))
   .forEach(file => {
+
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
     db[model.name] = model;
   });
 
 Object.keys(db).forEach(modelName => {
+
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
