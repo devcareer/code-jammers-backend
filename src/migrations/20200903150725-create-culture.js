1475d9e7289f27097e13a3cf551ca859f323eb57
AbonyiXavier <francisabonyi@gmail.com>
2020-09-14 22:43:48 +0100

    added changes

--- src/migrations/20200903150725-create-culture.js
+++ src/migrations/20200903150725-create-culture.js
@@ -1,53 +1,54 @@
 export default {
   up(queryInterface, Sequelize) {
     return queryInterface.sequelize
-      .query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp";')
-      .then(() =>
-        queryInterface.createTable("Cultures", {
-          id: {
-            allowNull: false,
-            primaryKey: true,
-            type: Sequelize.UUID,
-            defaultValue: Sequelize.literal("uuid_generate_v4()"),
+      .query("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";")
+      .then(() => queryInterface.createTable("Cultures", {
+        id: {
+          allowNull: false,
+          primaryKey: true,
+          type: Sequelize.UUID,
+          defaultValue: Sequelize.literal("uuid_generate_v4()"),
+        },
+        countryId: {
+          type: Sequelize.UUID,
+          references: {
+            model: "Countries",
+            key: "id",
           },
-          countryId: {
-            type: Sequelize.UUID,
-            references: { model: "Countries", key: "id" },
-          },
-          type: {
-            allowNull: false,
-            type: Sequelize.STRING,
-          },
-          festivals: {
-            allowNull: false,
-            type: Sequelize.STRING,
-          },
-          dressing: {
-            allowNull: false,
-            type: Sequelize.STRING,
-          },
-          language: {
-            allowNull: false,
-            type: Sequelize.STRING,
-          },
-          gallery: {
-            allowNull: false,
-            type: Sequelize.STRING,
-          },
-          tribe: {
-            allowNull: false,
-            type: Sequelize.STRING,
-          },
-          createdAt: {
-            allowNull: false,
-            type: Sequelize.DATE,
-          },
-          updatedAt: {
-            allowNull: false,
-            type: Sequelize.DATE,
-          },
-        })
-      );
+        },
+        type: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        festivals: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        dressing: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        language: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        gallery: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        tribe: {
+          allowNull: false,
+          type: Sequelize.STRING,
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      }));
   },
-  down: (queryInterface) => queryInterface.dropTable("Cultures"),
+  down: queryInterface => queryInterface.dropTable("Cultures"),
 };
--- src/seeders/20200904140351-culture.js
+++ src/seeders/20200904140351-culture.js
@@ -1,24 +1,22 @@
 export default {
-  up: (queryInterface) => {
-    return queryInterface.bulkInsert(
-      "Cultures",
-      [
-        {
-          id: "63995ef8-351f-4035-a268-c6cd7697f0ef",
-          countryId: "6003fb36-5112-463e-a1f9-c8944e72412f",
-          type: "Yoruba culture",
-          festivals: "Eyo festival",
-          dressing: "Agbádá àti Fìlà from Iseyin",
-          language: "Yoruba",
-          gallery:
+  up: queryInterface => queryInterface.bulkInsert(
+    "Cultures",
+    [
+      {
+        id: "63995ef8-351f-4035-a268-c6cd7697f0ef",
+        countryId: "6003fb36-5112-463e-a1f9-c8944e72412f",
+        type: "Yoruba culture",
+        festivals: "Eyo festival",
+        dressing: "Agbádá àti Fìlà from Iseyin",
+        language: "Yoruba",
+        gallery:
             "https://netstorage-legit.akamaized.net/images/468ff81348dddbff.jpg",
-          tribe: "Nigeria",
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
-  },
-  down: (queryInterface) => queryInterface.bulkDelete("Cultures", null, {}),
+        tribe: "Nigeria",
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ],
+    {},
+  ),
+  down: queryInterface => queryInterface.bulkDelete("Cultures", null, {}),
 };
