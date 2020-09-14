de8b7678c4372d85e3cfca72cd26c2d0621a111a
Uche <godspowercuche56@gmail.com>
2020-09-14 13:04:05 +0100

    add profile and user model

---
+++ src/models/profile.js
@@ -0,0 +1,21 @@
+module.exports = (sequelize, DataTypes) => {
+  const Profile = sequelize.define("Profile", {
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    profilePicture: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  });
+  return Profile;
+};
