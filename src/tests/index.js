d2c37fc3d374663ad2fc1fa3836848417159fc69
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-15 11:33:51 +0200

    feature(tourist-city-models) - create tourist center and city models
    - create city model
    - create tourist center model
    - create city migrations
    - create tourist centeer migrations
    - create city seeders
    - create tourist center seeders
    - install sequelize
    - install sequelize-cli
    
    [finishes #174626782]

--- src/tests/index.js
+++ src/tests/index.js
@@ -3,10 +3,7 @@ import "./unit/models/country.spec";
 import "./unit/models/culture.spec";
 import "./models/user";
 import "./models/profile";
-<<<<<<< HEAD
 import "./unit/models/food.spec";
 import "./unit/models/music.spec";
-=======
 import "./unit/models/City.spec";
 import "./unit/models/TouristCenter.spec";
->>>>>>> c6489c4... feature(tourist-city-models) - create tourist center and city models
