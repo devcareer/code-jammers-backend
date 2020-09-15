82dcf00efdfaa695d5090cc185229182c8e3c3c5
Augusta Ehihebolo <ehiheboloaugustar@gmail.com>
2020-09-15 13:28:28 +0100

    make correction

--- src/models/index.js
+++ src/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require("sequelize");
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = require(`${__dirname}/../../database/config.js`)[env];
 
 const db = {};
 
