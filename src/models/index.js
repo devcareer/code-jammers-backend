fabbed8d23794342964492a1757c18d763b3190d
Augusta Ehihebolo <ehiheboloaugustar@gmail.com>
2020-09-16 12:23:29 +0100

    resolve conflict

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,6 @@ const Sequelize = require("sequelize");
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = require(`${__dirname}/../database/config/config.js`)[env];
-const db = {};
 let sequelize;
 if (config.url) {
   sequelize = new Sequelize(config.url, config);
