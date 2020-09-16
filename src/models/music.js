43834e31916f636e45b377ac5ef0131ba9df31b6
Augusta Ehihebolo <ehiheboloaugustar@gmail.com>
2020-09-16 12:48:58 +0100

    resolve conflict

--- src/models/music.js
+++ src/models/music.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = (sequelize, DataTypes) => {
   const Music = sequelize.define("Music", {
     countryId: {
@@ -32,38 +31,3 @@ module.exports = (sequelize, DataTypes) => {
   };
   return Music;
 };
-=======
-module.exports = (sequelize, DataTypes) => {
-  const Music = sequelize.define("Music", {
-    countryId: {
-      type: DataTypes.UUID,
-      allowNull: false,
-      references: {
-        model: "Countries",
-        key: "id",
-      },
-    },
-    category: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    gallery: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    event: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-
-  });
-  Music.associate = (models) => {
-    Music.belongsTo(models.Country, {
-      foreignKey: {
-        allowNull: false,
-      },
-    });
-  };
-  return Music;
-};
->>>>>>> 523ffd3... make corrections
