bb252f901943b4842ada4f146577e482dc97aa90
Uche <godspowercuche56@gmail.com>
2020-09-16 12:00:06 +0100

    Resolve conflict

--- src/migrations/create-user.js
+++ src/migrations/create-user.js
@@ -1,4 +1,5 @@
 module.exports = {
+<<<<<<< HEAD
   up: (queryInterface, Sequelize) => queryInterface.createTable("Users", {
     id: {
       type: Sequelize.INTEGER,
@@ -33,3 +34,43 @@ module.exports = {
   }),
   down: (queryInterface) => queryInterface.dropTable("Users")
 };
+=======
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+      type: Sequelize.STRING,
+      allowNull:false,
+    },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Users');
+  }
+};
+>>>>>>> 5f3b0e5... feature(setup):crea user account
