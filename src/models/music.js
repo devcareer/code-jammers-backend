f883a0e41c9cce8357d593b2a6522b936e5c4830
Augusta Ehihebolo <ehiheboloaugustar@gmail.com>
2020-09-16 11:58:55 +0100

    resolve conflict

--- src/models/music.js
+++ src/models/music.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 module.exports = (sequelize, DataTypes) => {
   const Culture = sequelize.define("Music", {
     id: {
@@ -33,3 +34,28 @@ module.exports = (sequelize, DataTypes) => {
   };
   return Food;
 };
+=======
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Music extends Model {
+    
+    static associate(models) {
+     
+    }
+  };
+  Music.init({
+    id: DataTypes.INTEGER,
+    countryId: DataTypes.INTEGER,
+    category: DataTypes.STRING,
+    gallery: DataTypes.STRING,
+    event: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Music',
+  });
+  return Music;
+};
+>>>>>>> resolve conflict
