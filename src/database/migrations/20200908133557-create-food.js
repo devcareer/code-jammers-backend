b4962dca8fc52bede4dd16513b376805c704e813
Augusta Ehihebolo <ehiheboloaugustar@gmail.com>
2020-09-16 12:56:58 +0100

    resolve conflict

---
+++ src/database/migrations/20200908133557-create-food.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Food", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.literal("uuid_generate_v4()"),
+      },
+      countryId: { 
+        type: Sequelize.UUID, 
+        allowNull: false,
+      },
+      types: { 
+        allowNull: false, 
+        type: Sequelize.STRING,
+      },
+      methodOfPreparation: { 
+        allowNull: false, 
+        type: Sequelize.TEXT,
+      },
+      gallery: { 
+        allowNull: false, 
+        type: Sequelize.STRING,
+      },
+      createdAt: { 
+        allowNull: false, 
+        type: Sequelize.DATE 
+      },
+      updatedAt: { 
+        allowNull: false, 
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Food");
+  },
+};
