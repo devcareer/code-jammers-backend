ef0dfba456506a43d4d0ae72b3c76a88c29b2df3
Uche <godspowercuche56@gmail.com>
2020-09-17 15:22:36 +0100

    resolve conflicts

--- src/app.js
+++ src/app.js
@@ -1,8 +1,19 @@
 import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import userRoutes from "./routes/userRoutes";
 
 const app = express();
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
 
 const port = process.env.PORT || 3000;
+
+console.log(userRoutes);
+app.use("/api/v1/", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Welcome to Know Africa");
