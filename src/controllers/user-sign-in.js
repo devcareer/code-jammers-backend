41cf8ab3a49333b072c9f4289c2f8941e56c2503
donaldcrane <donaldagbakoba@gmail.com>
2020-09-17 11:34:10 +0100

    modified sign-in feature

---
+++ src/controllers/user-sign-in.js
@@ -0,0 +1,34 @@
+import bcrypt from "bcrypt";
+import Util from "../utilities/util";
+import User from "../UserService/User";
+import jwtHelper from "../UserService/Jwt";
+
+const { generateToken } = jwtHelper;
+const util = new Util();
+
+export default class loginController {
+  static async loginUser(req, res) {
+    try {
+      const { email, password } = req.body;
+      const userEmail = await User.checkEmail(email);
+      if (!userEmail) {
+        return res.status(400).send({ message: "Email adoes not exist." });
+      }
+      const validpass = await bcrypt.compare(password, userEmail.password);
+      if (!validpass) {
+        return res.status(400).send({ message: "Password does not exist." });
+      }
+      const token = await generateToken({ userEmail });
+      const data = {
+        email: userEmail.email,
+        password: userEmail.username
+      };
+      data.token = token;
+      util.setSuccess(201, "User logged in!", data);
+      return util.send(res);
+    } catch (error) {
+      util.setError(400, error.message);
+      return util.send(res);
+    }
+  }
+}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import userController from "../controllers/user";
+import loginController from "../controllers/user-sign-in";
 
 const router = Router();
 router.post("/users/signup", userController.createUser);
+router.post("/users/signin", loginController.loginUser);
 
 export default router;
