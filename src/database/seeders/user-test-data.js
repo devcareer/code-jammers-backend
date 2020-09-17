a64e079a44ba049e73bb50a381bdba602c8ae8f0
Uche <godspowercuche56@gmail.com>
2020-09-17 15:40:46 +0100

    fix conflicts

---
+++ src/database/seeders/user-test-data.js
@@ -0,0 +1,12 @@
+const user = {
+  username: "GarryT",
+  email: "Garrasdy@gmail.com",
+  password: "123456",
+};
+
+const user2 = {
+  email: "Garry@gmail.com",
+  password: "123456",
+};
+
+export { user, user2 };
