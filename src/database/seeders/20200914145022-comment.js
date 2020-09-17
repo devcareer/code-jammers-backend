4dbc192c4ff70ae45b05e70b169bbdc6bd21940d
Fiyin Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-09-17 10:43:36 +0100

    add hound.yml file

--- src/database/seeders/20200914145022-comment.js
+++ src/database/seeders/20200914145022-comment.js
@@ -1,11 +1,13 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert("Comments", [{
+      userId: "873c2726-877b-484c-8463-6299b8dc8ab1",
       comment: "Never knew Africa was not a country. Wow, thanks.",
       createdAt: new Date(),
       updatedAt: new Date(),
     },
     {
+      userId: "1b841d51-008a-43e0-ad17-d419b6ae6b00",
       comment: "I had a swell time visiting this city last year. Can't wait to visit with my family again.",
       createdAt: new Date(),
       updatedAt: new Date(),
