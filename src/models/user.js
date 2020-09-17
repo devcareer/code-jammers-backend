2542b15caf8764e4d88f21647a5d468e618fb764
Uche <godspowercuche56@gmail.com>
2020-09-17 16:11:53 +0100

    fix conflicts

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,17 @@
 export default (sequelize, DataTypes) => {
-  const User = sequelize.define("Users", {
+  const User = sequelize.define("User", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     username: {
       type: DataTypes.STRING,
@@ -14,9 +22,12 @@ export default (sequelize, DataTypes) => {
       allowNull: false,
     },
     role: {
-      type: DataTypes.ENUM("Super Admin", "Admin", "User"),
-      defaultValue: "User",
+      type: DataTypes.STRING,
+      defaultValue: "user",
     },
   });
+  // User.hasOne(models.Profile, {
+  //   onDelete: "cascade",
+  // });
   return User;
 };
