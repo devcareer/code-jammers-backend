8db4ecfbfb390edf025b23a2d7385f7e0beea3a1
Uche <godspowercuche56@gmail.com>
2020-09-17 16:17:50 +0100

    fix conflicts

---
+++ src/migrations/create-user.js
@@ -0,0 +1,39 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+      type: Sequelize.STRING,
+      allowNull:false,
+    },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface) => {
+    return queryInterface.dropTable('Users');
+  }
+};
