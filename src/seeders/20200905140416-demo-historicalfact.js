591dd56b5510c2a4b451b1d680cef24640338c42
Fiyin Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-09-18 12:14:25 +0100

    resolve conflicts

--- src/seeders/20200905140416-demo-historicalfact.js
+++ src/seeders/20200905140416-demo-historicalfact.js
@@ -1,10 +1,14 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
     await queryInterface.bulkInsert("Historicalfact", [{
 =======
     await queryInterface.bulkInsert("Hisoricalfact", [{
 >>>>>>> 825ba26... rebase develop
+=======
+    await queryInterface.bulkInsert("Historicalfact", [{
+>>>>>>> 3015f14... rebase develop
       countryId: 1,
       location: "Nigeria",
       gallery: "https://netstorage-legit.akamaized.net/images/468ff81348dddbff.jpg",
@@ -16,9 +20,13 @@ module.exports = {
 
   down: async (queryInterface, Sequelize) => {
 <<<<<<< HEAD
+<<<<<<< HEAD
     await queryInterface.bulkDelete("Historicalfact", null, {});
 =======
     await queryInterface.bulkDelete("Hisoricalfacts", null, {});
 >>>>>>> 825ba26... rebase develop
+=======
+    await queryInterface.bulkDelete("Historicalfact", null, {});
+>>>>>>> 3015f14... rebase develop
   },
 };
