3c5984f00724aba8ebc8d632935eb8deab30e8ea
Fiyin Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-09-18 11:57:27 +0100

    resolve conflicts

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -10,20 +10,6 @@ chai.should();
 chai.use(chaiHttp);
 
 describe("app.js should return all endpoints", async () => {
-<<<<<<< HEAD
-  describe("/ should display Welcome to Know Africa", () => {
-    it("it should get the welcome page", done => {
-      chai
-        .request(server)
-        .get("/")
-        .end((err, res) => {
-          res.should.have.status(200);
-          done();
-        });
-    });
-  });
-});
-=======
 	describe("/ should display Welcome to Know Africa", () => {
 		it("it should get the welcome page", (done) => {
 			chai
@@ -38,4 +24,3 @@ describe("app.js should return all endpoints", async () => {
 });
 
 export default describe;
->>>>>>> 080040d... feat(models): Design historical & security models
