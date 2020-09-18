b2bab4c36830f9e9adae960fa568b76a7957d41a
Fiyin Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-09-18 11:52:51 +0100

    resolve conflicts

--- src/migrations/20200904193019-create-securityinformation.js
+++ src/migrations/20200904193019-create-securityinformation.js
@@ -1,38 +1,38 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Securityinformation", {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      countryId: {
-        type: Sequelize.INTEGER,
-      },
-      location: {
-        type: Sequelize.STRING,
-      },
-      // countryId: {
-      //   type: Sequelize.INTEGER,
-      //   onDelete: "CASCADE",
-      //   allowNull: false,
-      //   references: {
-      //     model: "Country",
-      //     key: "id",
-      //   },
-      // },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("Securityinformation");
-  },
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("Securityinformations", {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER,
+			},
+			countryId: {
+				type: Sequelize.INTEGER,
+			},
+			location: {
+				type: Sequelize.STRING,
+			},
+			// countryId: {
+			// 	type: Sequelize.INTEGER,
+			// 	onDelete: "CASCADE",
+			// 	allowNull: false,
+			// 	references: {
+			// 		model: "Countries",
+			// 		key: "id",
+			// 	},
+			// },
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("Securityinformations");
+	},
 };
