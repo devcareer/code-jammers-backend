31c9b81c459936538b227cc0d77bbeec245cd8a4
Uche <godspowercuche56@gmail.com>
2020-09-21 00:36:51 +0100

    resolving conflicts

--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,9 @@
-<<<<<<< HEAD
-<<<<<<<< HEAD:src/models/User.js
-export default (sequelize, DataTypes) => {
-  const User = sequelize.define("Users", {
-=======
 export default (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
->>>>>>> c7f7c44... resolve conflicts
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-<<<<<<< HEAD
-=======
-      validate: {
-        isEmail: true,
-      },
->>>>>>> c7f7c44... resolve conflicts
     },
     username: {
       type: DataTypes.STRING,
@@ -31,56 +14,14 @@ export default (sequelize, DataTypes) => {
       allowNull: false,
     },
     role: {
-<<<<<<< HEAD
       type: DataTypes.ENUM("Super Admin", "Admin", "User"),
-      defaultValue: "User",
-    },
-  });
-  return User;
-};
-========
-export default (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: {
-      type: DataTypes.STRING,
       defaultValue: "user",
     },
   });
-  // User.hasOne(models.Profile, {
-  //   onDelete: "cascade",
-  // });
+  User.associate = models => {
+    User.belongsTo(models.Profiles, {
+      as: "profileDetails", foreignKey: "userId",
+    });
+  };
   return User;
 };
->>>>>>>> c7f7c44... resolve conflicts:src/models/user.js
-=======
-      type: DataTypes.STRING,
-      defaultValue: "user",
-    },
-  });
-  // User.hasOne(models.Profile, {
-  //   onDelete: "cascade",
-  // });
-  return User;
-};
->>>>>>> c7f7c44... resolve conflicts
