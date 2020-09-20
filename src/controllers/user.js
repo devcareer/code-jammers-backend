8c1647faea07e3927a29d4fc44d3f562b1783af0
Uche <godspowercuche56@gmail.com>
2020-09-21 00:57:45 +0100

    resolving conflicts

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import bcrypt from "bcrypt";
 import Util from "../utilities/util";
 import User from "../services/UserService/User";
@@ -29,10 +30,32 @@ export default class userController {
       };
       data.token = token;
       util.setSuccess(201, "User created!", data);
+=======
+import util from "../utilities/util";
+import User from "../UserService/User";
+
+class userController {
+  static async createUser(req, res) {
+    const { email, username, password } = req.body;
+    if (!email || !username || !password) {
+      util.setError(400, "Please you are required to fill all fields");
+      return util.send(res);
+    }
+    const newUser = req.body;
+    try {
+      const createdUser = await User.createUser(newUser);
+      util.setSuccess(201, "User created!", createdUser);
+>>>>>>> 283f3f5... resolve conflict
       return util.send(res);
     } catch (error) {
       util.setError(400, error.message);
       return util.send(res);
     }
   }
+<<<<<<< HEAD
 }
+=======
+}
+
+export default userController;
+>>>>>>> 283f3f5... resolve conflict
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,6 +2,11 @@ import { Router } from "express";
 import userController from "../controllers/user";
 
 const router = Router();
+<<<<<<< HEAD
 router.post("/users/signup", userController.createUser);
+=======
+
+router.post("/", userController.createUser);
+>>>>>>> 283f3f5... resolve conflict
 
 export default router;
