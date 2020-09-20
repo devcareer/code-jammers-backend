30659f8024f3302abab6f03067d75dbb8b8d58c4
Uche <godspowercuche56@gmail.com>
2020-09-21 00:47:03 +0100

    resolving conflicts

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export default (sequelize, DataTypes) => {
   const User = sequelize.define("User", {
     email: {
@@ -26,38 +25,3 @@ export default (sequelize, DataTypes) => {
   };
   return User;
 };
-=======
-export default (sequelize, DataTypes) => {
-  const User = sequelize.define("User", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-      validate: {
-        isEmail: true,
-      },
-    },
-    username: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    role: {
-      type: DataTypes.STRING,
-      defaultValue: "user",
-    },
-  });
-  // User.hasOne(models.Profile, {
-  //   onDelete: "cascade",
-  // });
-  return User;
-};
->>>>>>> 07b9634... resolve conflicts
