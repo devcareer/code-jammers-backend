2bb10b07d6e0d6065afad514b7d7b73d20c4d113
Uche <godspowercuche56@gmail.com>
2020-09-21 00:48:53 +0100

    resolving conflicts

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
+  
 import bcrypt from "bcrypt";
 import Util from "../utilities/util";
-<<<<<<< HEAD
 import User from "../services/UserService/User";
 import jwtHelper from "../utilities/Jwt";
 import { registerValidation } from "../validation/userValidation";
@@ -30,34 +30,10 @@ export default class userController {
       };
       data.token = token;
       util.setSuccess(201, "User created!", data);
-=======
-import User from "../UserService/user";
-
-const util = new Util();
-
-class userController {
-  static async createUser(req, res) {
-    const { email, username, password } = req.body;
-    if (!email || !username || !password) {
-      util.setError(400, "Please you are required to fill all fields");
-      return util.send(res);
-    }
-    const hashedPassword = bcrypt.hashSync(password);
-    console.log(hashedPassword);
-    const newUser = req.body;
-    try {
-      const createdUser = await User.createUser(newUser);
-      util.setSuccess(201, "User created!", createdUser);
->>>>>>> 07b9634... resolve conflicts
       return util.send(res);
     } catch (error) {
       util.setError(400, error.message);
       return util.send(res);
     }
   }
-<<<<<<< HEAD
 }
-=======
-}
-export default userController;
->>>>>>> 07b9634... resolve conflicts
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,6 @@ import { Router } from "express";
 import userController from "../controllers/user";
 
 const router = Router();
-<<<<<<< HEAD
-=======
-console.log(userController.createUser);
->>>>>>> 07b9634... resolve conflicts
 router.post("/users/signup", userController.createUser);
 
 export default router;
