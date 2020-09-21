35300190a51a91ba430ec825c6c9b825c0155713
Uche <godspowercuche56@gmail.com>
2020-09-21 01:25:03 +0100

    resolving conflicts

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 import Joi from "joi";
 
-<<<<<<< HEAD
 const registerValidation = user => {
   const schema = Joi.object({
-    username: Joi.string().min(5).max(50),
+    username: Joi.string().min(3).max(30).required(),
     email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uk", "co"] } }).min(5)
       .max(255),
     password: Joi.string().required().min(5).max(1024),
@@ -13,44 +11,3 @@ const registerValidation = user => {
 };
 
 export { registerValidation };
-=======
-export default class userValidation {
-  static get userSchema() {
-    return Joi.object({
-      username: Joi.string().min(3).max(20).required(),
-      email: Joi.string().email().min(3).max(100),
-      password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-    });
-  }
-}
->>>>>>> 84d76fc... add email conflict test
-=======
-// import Joi from "joi";
-
-// export default class userValidation {
-//   static get userSchema() {
-//     return Joi.object({
-//       username: Joi.string().min(3).max(20).required(),
-//       email: Joi.string().email().min(3).max(100),
-//       password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
-//     });
-//   }
-// };
-
-import Joi from "@hapi/joi";
-
-const userValidation = data => {
-  const schema = {
-    username: Joi.string().min(4).max(15).required(),
-    email: Joi.string().email().min(3).max(150),
-    password: Joi.string().min(6).max(100).required(),
-  };
-  console.log(`data:${data}`);
-  console.log(`schema: ${schema}`);
-  return Joi.validate(data, schema);
-};
-
-export default userValidation;
-
-// module.exports.userValidation = userValidation;
->>>>>>> 5a6ba69... add validation
