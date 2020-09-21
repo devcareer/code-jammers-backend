8e8a1304cb328dbc52d3144f9196045ab03ebd98
Uche <godspowercuche56@gmail.com>
2020-09-21 01:19:25 +0100

    resolving conflicts

--- src/tests/models/user.spec.js
+++ src/tests/models/user.spec.js
@@ -1,27 +1,22 @@
-import chai, { expect } from "chai";
-
-import sinonChai from "sinon-chai";
-import {
-  sequelize,
-  dataTypes,
-  checkModelName,
-  checkPropertyExists,
-} from "sequelize-test-helpers";
-
-<<<<<<< HEAD
-import UserModel from "../../models/User";
-=======
-import UserModel from "../../models/user";
->>>>>>> e6f281f... resolve merge conflict
-
-chai.use(sinonChai);
-describe("src/models/User", () => {
-  const User = UserModel(sequelize, dataTypes);
-  const user = new User();
-  checkModelName(User)("Users");
-  context("properties", () => {
-    ["email", "username", "password", "role"].forEach(
-      checkPropertyExists(user),
-    );
+export default (sequelize, DataTypes) => {
+  const User = sequelize.define("Users", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.ENUM("Super Admin", "Admin", "User"),
+      defaultValue: "User",
+    },
   });
-});
+  return User;
+};
