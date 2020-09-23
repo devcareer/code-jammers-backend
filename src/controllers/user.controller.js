ec9f86599b90c3e45990d4034dfbb53facfaf275
mdcodes <mdcodes2308@gmail.com>
2020-09-23 12:55:30 +0100

    refactor code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,14 @@
 import { Users, Profiles } from "../models";
 
+// eslint-disable-next-line require-jsdoc
+function filterValues(obj) {
+  Object.keys(obj).forEach(key => (obj[key] === null
+    || obj[key] === undefined
+    || obj[key] === ""
+    // eslint-disable-next-line no-param-reassign
+    ? delete obj[key]
+    : {}));
+}
 exports.editProfile = (req, res) => {
   console.log(req.body);
   const { email, username, password } = req.body;
@@ -9,11 +18,7 @@ exports.editProfile = (req, res) => {
 
   if (email || username || password) {
     const updateUser = { email, username, password };
-    // eslint-disable-next-line max-len
-    Object.keys(updateUser).forEach(key => (updateUser[key] === null || updateUser[key]
-      === undefined || updateUser[key] === "" ? delete updateUser[key]
-      : {}));
-
+    filterValues(updateUser);
     Users.update(updateUser, {
       where: {
         username: req.params.username,
@@ -33,11 +38,7 @@ exports.editProfile = (req, res) => {
             lastName: lastname,
             profilePicture: profilepicture,
           };
-          // eslint-disable-next-line max-len
-          Object.keys(updateProfile).forEach(key => (updateProfile[key] === null || updateProfile[key] === undefined
-            || updateProfile[key] === ""
-            ? delete updateProfile[key]
-            : {}));
+          filterValues(updateProfile);
           Users.findOne({
             where: {
               username: req.params.username,
--- src/models/index.js
+++ src/models/index.js
@@ -18,9 +18,10 @@ if (config.url) {
     config,
   );
 }
-fs
-  .readdirSync(__dirname)
-  .filter(file => (file.indexOf(".") !== 0) && (file !== basename) && (file.slice(-3) === ".js"))
+fs.readdirSync(__dirname)
+  .filter(
+    file => file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js",
+  )
   .forEach(file => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
