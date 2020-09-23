7e7ee8a964f5a69de97cc9cd171f5b6e4a429c36
mdcodes <mdcodes2308@gmail.com>
2020-09-23 12:14:54 +0100

    refactor code

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -8,14 +8,12 @@ exports.editProfile = (req, res) => {
   let editedProfile = {};
 
   if (email || username || password) {
-    let updateUser = { email, username, password };
-    Object.keys(updateUser).forEach((key) =>
-      updateUser[key] === null ||
-      updateUser[key] === undefined ||
-      updateUser[key] === ""
-        ? delete updateUser[key]
-        : {}
-    );
+    const updateUser = { email, username, password };
+    Object.keys(updateUser).forEach(key => (updateUser[key] === null
+      || updateUser[key] === undefined
+      || updateUser[key] === ""
+      ? delete updateUser[key]
+      : {}));
 
     Users.update(updateUser, {
       where: {
@@ -24,32 +22,30 @@ exports.editProfile = (req, res) => {
       returning: true,
       plain: true,
     })
-      .then((result) => {
+      .then(result => {
         console.log(result[1].dataValues);
         editedUser = result[1].dataValues;
       })
       .then(() => {
         const { firstname, lastname, profilepicture } = req.body;
         if (firstname || lastname || profilepicture) {
-          let updateProfile = {
+          const updateProfile = {
             firstName: firstname,
             lastName: lastname,
             profilePicture: profilepicture,
           };
-          Object.keys(updateProfile).forEach((key) =>
-            updateProfile[key] === null ||
-            updateProfile[key] === undefined ||
-            updateProfile[key] === ""
-              ? delete updateProfile[key]
-              : {}
-          );
+          Object.keys(updateProfile).forEach(key => (updateProfile[key] === null
+            || updateProfile[key] === undefined
+            || updateProfile[key] === ""
+            ? delete updateProfile[key]
+            : {}));
           Users.findOne({
             where: {
               username: req.params.username,
             },
           })
-            .then((user) => {
-              let id = user.id;
+            .then(user => {
+              const { id } = user;
               Profiles.update(updateProfile, {
                 where: {
                   userId: id,
@@ -57,20 +53,18 @@ exports.editProfile = (req, res) => {
                 returning: true,
                 plain: true,
               })
-                .then((result) => {
+                .then(result => {
                   console.log(result[1].dataValues);
                   editedProfile = result[1].dataValues;
                   res.status(200).json({ editedUser, editedProfile });
                 })
-                .catch((error) =>
-                  res.status(403).json({ message: error.message })
-                );
+                .catch(error => res.status(403).json({ message: error.message }));
             })
-            .catch((error) => res.status(403).json({ message: error.message }));
+            .catch(error => res.status(403).json({ message: error.message }));
         } else {
           res.status(200).json({ editedUser });
         }
       })
-      .catch((error) => res.status(403).json({ message: error.message }));
+      .catch(error => res.status(403).json({ message: error.message }));
   }
 };
