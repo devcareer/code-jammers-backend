6ee88770c5752ba23e3dbccf9713311fda539376
Augusta Ehihebolo <ehiheboloaugustar@gmail.com>
2020-09-24 12:40:16 +0100

    one commit

--- src/database/config/facebookpassport.js
+++ src/database/config/facebookpassport.js
@@ -1,9 +1,8 @@
 module.exports = {
-    facebook: {
-        clientID: FACEBOOK_APP_ID,
-        clientSecret: FACEBOOK_APP_SECRET,
-        callbackURL: "http://www.example.com/auth/facebook/callback"
+  facebook: {
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://www.example.com/auth/facebook/callback"
 
-    }
-}
- 
+  }
+};
