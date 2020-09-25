eb7eb9996ec4120c614c8b8e763794337dee4221
Bernard-Namangala <bernardnamangala@gmail.com>
2020-09-25 10:29:33 +0200

    feature(get country routes): create get country routes
    - create get single country route
    - create get multiple countries routes
    - create get single country controller
    - create get multiple countries controller
    
    [Finishes #174904959]

--- src/models/index.js
+++ src/models/index.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
+
 const config = require(`${__dirname}/../database/config/config.js`)[env];
 const db = {};
 let sequelize;
