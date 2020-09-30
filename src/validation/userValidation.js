142976f6f6d5be5125fc3f2d0d01481e0ebc619a
Agbakoba Donald <61365997+donaldcrane@users.noreply.github.com>
2020-09-30 10:54:16 +0100

    #174566162 Build out endpoint for user sign in (#27)
    feat(sign-in): a user can sign in
    
    - build out the endpoint for user sign in
    - write tests for the user sign-in endpoint
    - write validation for user sign-in fields and process
    
    [Finishes #174566162]

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -28,4 +28,31 @@ const registerValidation = user => {
   return schema.validate(user);
 };
 
-export { registerValidation };
+const loginValidation = user => {
+  const schema = Joi.object({
+    username: Joi.string().alphanum().min(3)
+      .max(255)
+      .empty()
+      .messages({
+        "string.alphanum": "Sorry, Username is not required",
+        "string.empty": "Sorry, username is not required",
+      }),
+    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uk", "co", "io"] } }).min(5)
+      .max(100)
+      .empty()
+      .messages({
+        "string.email": "Please enter a valid email",
+        "string.empty": "Sorry, Email cannot be an empty field",
+      }),
+    password: Joi.string().required().min(5).max(1024)
+      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .messages({
+        "string.pattern.base": "Password must contain only alphanumeric characters.",
+        "string.empty": "Sorry, password cannot be an empty field",
+        "string.min": "Password should have a minimum length of 5"
+      }),
+  });
+  return schema.validate(user);
+};
+
+export { registerValidation, loginValidation };
