779f2a30f870b9ffcd8f4533cda8e8db17bb8c39
Fiyinfoluwa Akinsiku <fiyinakinsiku.b@gmail.com>
2020-10-01 03:34:34 +0100

    fix codeclimate error

--- src/services/AdminServices/countryService.js
+++ src/services/AdminServices/countryService.js
@@ -4,16 +4,16 @@ export default class Admin {
   static async addCountry(newCountry) {
     try {
       return await database.Countries.create(newCountry);
-    } catch (error) {
-      throw error;
+    } catch (err) {
+      throw err;
     }
   }
 
   static async checkCountry(nameOfCountry) {
     try {
       return await database.Countries.findOne({ where: { nameOfCountry } });
-    } catch (error) {
-      throw error;
+    } catch (err) {
+      throw err;
     }
   }
 }
