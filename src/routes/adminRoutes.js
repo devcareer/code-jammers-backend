17ab6ed6a2098b83931afeb4db4c2dc3384c6c07
Fiyin Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-10-08 10:55:04 +0100

    destructure addcountry

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -3,7 +3,9 @@ import adminController from "../controllers/addCountry";
 import loggedIn from "../middlewares/loggedIn";
 import isAdmin from "../middlewares/isAdmin";
 
+const { addCountry } = adminController;
+
 const router = Router();
-router.post("/admin/addcountry", loggedIn, isAdmin, adminController.addCountry);
+router.post("/admin/addcountry", loggedIn, isAdmin, addCountry);
 
 export default router;
