0fe1849d11587f49cbcab5def456f5ac5b6ba987
Fiyin Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-10-08 14:07:25 +0100

    modify node action

--- src/models/index.js
+++ src/models/index.js
@@ -1,10 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-require('dotenv').config;
+require('dotenv').config();
 
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV;
 const config = require(`${__dirname}/../database/config/config.js`)[env];
 const db = {};
 let sequelize;
