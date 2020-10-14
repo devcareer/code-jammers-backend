44a08450ecc341a07926e6a289eeda6b97667a3a
Bislon Zulu <bislonzulu1@outlook.com>
2020-10-14 14:18:56 +0200

    refactor code to follow standards

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,10 +1,1 @@
 import "./util";
-import sendEmail from "./sendEmail";
-<<<<<<< HEAD
-
-export default { sendEmail };
-=======
-import randomTokenGenerator from "./randomTokenGenerator";
-
-export default { sendEmail, randomTokenGenerator };
->>>>>>> 23c2935... add test files for reset password
