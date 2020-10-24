7b36010755ed9fa7d29fc531cdfbb2e1bdba7f82
MIIELD <mdcodes>
2020-10-24 12:29:54 +0100

    solve conflict

--- src/validation/userValidation.js
+++ src/validation/userValidation.js
@@ -1,62 +1,41 @@
-import Joi from "joi";
+export default class Util {
+  constructor() {
+    this.statusCode = null;
+    this.type = null;
+    this.data = null;
+    this.token = null;
+    this.message = null;
+    this.error = null;
+  }
 
-const registerValidation = user => {
-  const schema = Joi.object({
-    username: Joi.string().required().alphanum().min(3)
-      .max(255)
-      .empty()
-      .messages({
-        "any.required": "Sorry, username is required",
-        "string.alphanum": "Sorry, Username must contain only alphanumeric characters",
-        "string.empty": "username cannot be an empty field",
-        "string.min": "username should have a minimum length of 3 and a maximum length of 255"
-      }),
-    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uk", "co"] } }).min(5)
-      .max(100)
-      .empty()
-      .messages({
-        "any.required": "Sorry, email is required",
-        "string.empty": "Sorry, Email cannot be an empty field",
-        "string.email": "Please enter a valid email",
-      }),
-    password: Joi.string().required().empty().min(5)
-      .max(1024)
-      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
-      .messages({
-        "any.required": "Sorry, password is required",
-        "string.pattern.base": "password must contain only alphanumeric characters.",
-        "string.empty": "Sorry, password cannot be an empty field",
-        "string.min": "password should have a minimum length of 5"
-      }),
-  }).options({ abortEarly: false });
-  return schema.validate(user);
-};
-const loginValidation = user => {
-  const schema = Joi.object({
-    username: Joi.string().alphanum().min(3)
-      .max(255)
-      .empty()
-      .messages({
-        "string.alphanum": "Sorry, Username is not required",
-        "string.empty": "Sorry, username is not required",
-      }),
-    email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: ["com", "net", "uk", "co", "io"] } }).min(5)
-      .max(100)
-      .empty()
-      .messages({
-        "any.required": "Sorry, email is required",
-        "string.email": "Please enter a valid email",
-        "string.empty": "Sorry, Email cannot be an empty field",
-      }),
-    password: Joi.string().required().min(5).max(1024)
-      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
-      .messages({
-        "string.pattern.base": "Password must contain only alphanumeric characters.",
-        "string.empty": "Sorry, password cannot be an empty field",
-        "string.min": "Password should have a minimum length of 5"
-      }),
-  });
-  return schema.validate(user);
-};
+  setSuccess(statusCode, message, token, data) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.token = token;
+    this.data = data;
+    this.type = "success";
+  }
 
-export { registerValidation, loginValidation };
+  setError(statusCode, message, error) {
+    this.statusCode = statusCode;
+    this.message = message;
+    this.error = error;
+    this.type = "error";
+  }
+
+  send(res) {
+    const result = {
+      status: this.statusCode,
+      message: this.message,
+      data: this.data,
+      token: this.token,
+    };
+    if (this.type === "success") {
+      return res.status(this.statusCode).json(result);
+    }
+    return res.status(this.statusCode).json({
+      status: this.statusCode,
+      error: this.error
+    });
+  }
+}
