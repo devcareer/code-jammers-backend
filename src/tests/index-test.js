1da2ac42b8558a8808187ae42ecabcbd749c96a3
Fiyinfoluwa Akinsiku <fiyinfoluwaakinsiku@yahoo.com>
2020-10-26 09:58:22 +0100

    #174879639 Build Api for admin to add a country (#36)
    feat(addCountry): admin can add a country
    
    - build out the api for admin to add a country
    - add jsdoc comments to every function in the codebase
    
    [Delivers #174879639]

--- src/tests/index-test.js
+++ src/tests/index-test.js
@@ -2,8 +2,6 @@ import chai from "chai";
 import chaiHttp from "chai-http";
 import server from "../app";
 
-// assertion style
-
 chai.should();
 
 chai.use(chaiHttp);
--- src/validation/index.js
+++ src/validation/index.js
@@ -1,1 +1,2 @@
 import "./userValidation";
+import "./countryValidation";
