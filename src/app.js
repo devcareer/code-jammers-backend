8ff54d5126ad395a3ade8126d390c18fd95b28be
donaldcrane <donaldagbakoba@gmail.com>
2020-11-02 17:50:13 +0100

    resolved conflict

--- src/app.js
+++ src/app.js
@@ -12,9 +12,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(cors());
 
 app.use("/api/v1", countryRoutes);
