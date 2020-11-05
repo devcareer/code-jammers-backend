3af9a7319d9896493ab98db7e4d46c50cb4e750f
Godspower <Godspowercuche56@gmail.com>
2020-11-05 22:59:06 +0100

    Rebase developrebase develop

--- src/routes/foodRoute/foodRoutes.js
+++ src/routes/foodRoute/foodRoutes.js
@@ -15,8 +15,8 @@ const router = Router();
 router.post("/admin/food", verifyToken, verifyAdmin, createFood);
 router.patch("/admin/food/:id", verifyToken, verifyAdmin, updateFood);
 router.delete("/admin/food/:id", verifyToken, verifyAdmin, deleteFood);
-router.get("/food/country-foods", getFoodsByCountry);
 router.get("/food", getAllFoods);
+router.get("/food/country-foods/:country", getFoodsByCountry);
 router.get("/food/:id", getOneFoodById);
 
 export default router;
