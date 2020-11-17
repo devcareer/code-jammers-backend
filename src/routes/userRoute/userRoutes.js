1b2edf754d6237e372aeb77b7f7a369cad5609f0
unknown <ufuomaogodo@gmail.com>
2020-11-17 11:37:45 +0100

    add changes

--- src/routes/userRoute/userRoutes.js
+++ src/routes/userRoute/userRoutes.js
@@ -7,8 +7,19 @@ const router = Router();
 const { verifyToken, verifyUserById } = Authentication;
 
 router.post("/users/signup", userController.createUser);
-router.get("/auth/google/signup", passport.authenticate("googleSignUp", {scope: ["profile", "email"],}));
-router.get("/auth/google/signin", passport.authenticate("googleSignIn", {scope: ["profile", "email"],}));
+router.get(
+  "/auth/google/signup",
+  passport.authenticate("googleSignUp", {
+    scope: ["profile", "email"],
+  })
+);
+
+router.get(
+  "/auth/google/signin",
+  passport.authenticate("googleSignIn", {
+    scope: ["profile", "email"],
+  })
+);
 router.patch("/user-profile/", verifyToken, verifyUserById, userController.updateUserProfile);
 router.get("/users/signup/verify/:email", userController.verifyUser);
 router.post("/users/signin", userController.loginUser);
